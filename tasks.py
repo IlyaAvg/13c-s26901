fbd573183e41f2c72161a9baa8fd45c43d286a4b
IlyaAvg <162124013+IlyaAvg@users.noreply.github.com>
2024-03-18 12:39:42 +0100

    task5

--- tasks.py
+++ tasks.py
@@ -26,3 +26,13 @@ class SquareGenerator:
 
 list = SquareGenerator.generate_squares(SquareGenerator, 1, 10)
 print("Task 4 - Libraries: " + str(list))
+
+# Task 5
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        if end < start:
+            raise ValueError("End of the range must be greater than or equal to start.")
+        return [math.pow(x,2) for x in range(start,end+1)]
+
+list = SquareGenerator.generate_squares(SquareGenerator, 1, 10)
+print("Task 5 - Exceptions: " + str(list))
