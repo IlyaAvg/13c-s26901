3097db0e5ff3a68979befebc6fb3fed89cc265e2
IlyaAvg <162124013+IlyaAvg@users.noreply.github.com>
2024-03-18 12:34:22 +0100

    task3

--- tasks.py
+++ tasks.py
@@ -6,3 +6,11 @@ print("Task 1 - List Comprehensions:", squares_list)
 def generate_squares(start, end):
     return [x**2 for x in range(start, end+1)]
 print("Task 2 - Functions:", generate_squares(1, 10))
+
+# Task 3
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        return [x**2 for x in range(start, end+1)]
+
+list = SquareGenerator.generate_squares(SquareGenerator, 1,10)
+print("Task 3 - Classes: "+str(list))
